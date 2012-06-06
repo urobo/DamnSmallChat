4c5fe34e15833760f9bd7ba7f768645b110e1f47
urobo <rico.sleeps@gmail.com>
2012-06-06 15:49:41 +0200

    few improvements

--- src/it/unibz/DamnSmallChat/DamnSmallUDPChat.java
+++ src/it/unibz/DamnSmallChat/DamnSmallUDPChat.java
@@ -2,22 +2,23 @@ package it.unibz.DamnSmallChat;
 
 public class DamnSmallUDPChat {
 	private static final String LOCALHOST = "localhost";
-	private static final String DEBUG_NICKNAME = "debug";
-	private static final String PORT = "1833";
+	
 	private static final String DEFAULT_NAME = "Default Channel";
-	private static final String DEBUG_ARGS[] = { PORT, LOCALHOST,
-			DEBUG_NICKNAME, PORT };
+	private static final String DEBUG_NICKNAME1 = "debug1";
+	private static final String DEBUG_NICKNAME2 = "debug2";
+	private static final String PORT1 = "1833";
+	private static final String PORT2 = "1834";
+	
+	private static final String DEBUG_ARGS1[] = { PORT2, LOCALHOST,
+		DEBUG_NICKNAME1, PORT1 };
+	private static final String DEBUG_ARGS2[] = { PORT1, LOCALHOST,
+		DEBUG_NICKNAME2, PORT2 };
 
 	public static void main(String args[]) {
 		if (args.length < 4) {
 			System.out
 					.println("usage java DamnSmallUDPChat <RemoteClientServerPort::int> <RemoteClientServerAddress::String> <Nickname::String> <LocalClientServerPort::int>");
-			System.out.println("using static Args for debug");
-
-			args = new String[4];
-			for (int i = 0; i < DEBUG_ARGS.length; i++) {
-				args[i] = DEBUG_ARGS[i];
-			}
+			args = DEBUG_ARGS1;
 		}
 		Channel currentChannel = new Channel(new PrintService(DEFAULT_NAME));
 
